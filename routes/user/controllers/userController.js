6142e317952a19eda05d738db8f1c3b069e460eb
Mintae Lee <lee.mintae@codeimmersives.com>
2019-09-19 11:08:42 -0400

    fixed error in signin/signup controller

--- routes/user/controllers/userController.js
+++ routes/user/controllers/userController.js
@@ -3,22 +3,22 @@ const argon2 = require('argon2')
 const jwt = require('jsonwebtoken')
 
 module.exports = {
-    signup: async (params) => {
+    signup: async (req, res, next) => {
         try {
-            const user = await User.findOne({email: params.email})
+            const user = await User.findOne({email: req.body.email})
                 if (user){
                     console.log('user exists')
                     let error = {}
                     error.status = 400
                     error.confirmation = false
                     error.message = 'User with same email address already exists!'
-                    return error
+                    res.json(error)
                 } else {
                     try {
-                        const hash = await argon2.hash(params.password)
+                        const hash = await argon2.hash(req.body.password)
                         const newUser = new User({
-                            email: params.email,
-                            username: params.username,
+                            email: req.body.email,
+                            username: req.body.username,
                             password: hash
                         })
                         const createdUser = await newUser.save()
@@ -34,34 +34,31 @@ module.exports = {
                                 let error = {}
                                 error.status = 400
                                 error.confirmation = false
-                                error.message = '1'
-                                return error
+                                res.json(error)
                             } else {
                                 let success = {}
                                 success.confirmation = true
                                 success.token = `Bear ${token}`
-                                return success
+                                res.json(success)
                             }
                         })
                     } catch (err) {
                         let error = {}
                         error.status = 400
                         error.confirmation = false
-                        error.message = '2'
-                        return error
+                        res.json(error)
                     }
                 }
         } catch (err) {
             let error = {}
             error.status = 400
             error.confirmation = false
-            error.message = '3'
-            return error
+            res.json(error)
         }
     },
-    signin: async (params) => {
-        const email = params.email
-        const password = params.password
+    signin: async (req, res, next) => {
+        const email = req.body.email
+        const password = req.body.password
         try{
             const user = await User.findOne({ email })
             if (!user){
@@ -69,7 +66,7 @@ module.exports = {
                 error.status = 400
                 error.confirmation = false
                 error.message = 'User not found!'
-                return error
+                res.json(error)
             } else {
                 try{
                     if (await argon2.verify(user.password, password)){
@@ -78,22 +75,20 @@ module.exports = {
                             email: user.email,
                             username: user.username
                         }
-                        await jwt.sign(payload, process.env.SECRET_KEY, {
+                        jwt.sign(payload, process.env.SECRET_KEY, {
                             expiresIn: 3600
-                        }, async (err, token) => {
+                        }, (err, token) => {
                             if (err){
                                 let error = {}
                                 error.status = 400
                                 error.confirmation = false
                                 error.message = err
-                                return error
+                                res.json(error)
                             } else {
                                 let success = {}
                                 success.confirmation = true
                                 success.token = `Bearer ${token}`
-                                console.log('94')
-                                console.log("success before return: ", success)
-                                return await returnSuccess(success)
+                                res.json(success)
                             }
                         })
                     }
@@ -102,16 +97,15 @@ module.exports = {
                     error.message = 'Incorrect username or password'
                     error.confirmation = false
                     error.status = 400
-                    return error
+                    res.json(error)
                 }
             }
         } catch (err){
             let error = {}
             error.confirmation = false
             error.status = 400
-            return error
+            res.json(error)
         }
     }
 }
 
-const returnSuccess = success => Promise.resolve(success)
